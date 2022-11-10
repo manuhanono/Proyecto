7abb64a5b907bc292281f743e8563685eb465988
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-10 17:36:09 -0300

    Update ProyectoI.py

--- ProyectoI.py
+++ ProyectoI.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #DATA_URL = ("https://github.com/chairielazizi/streamlit-collision/blob/master/Motor_Vehicle_Collisions_-_Crashes.csv?raw=true")
 
-DATA_URL1 = ("https://github.com/manuhanono/infovis/blob/main/crashes.parquet?raw=true")
+DATA_URL1 = ("https://github.com/manuhanono/Proyecto/blob/main/crashes2.parquet?raw=true)
 
 
 @st.cache(persist=True)
@@ -13,27 +13,9 @@ def load_data(rows):
     data = data.head(rows)
     return data
 
-data = load_data(100000)
+data = load_data(1950000)
 
-#Manipulacion del DATASET extra
-data = data.rename(columns={"NUMBER OF PERSONS INJURED": "PINJ", "LATITUDE": "lat", "LONGITUDE": "lon", "CRASH DATE": "DATE"})
-data['YEAR'] = pd.DatetimeIndex(data['DATE']).year
-data['MONTH'] = pd.DatetimeIndex(data['DATE']).month
-data['CANT_AUTOS'] = 5
-data = data.replace({pd.NA: ""})
 
-for i in range(len(data)):
-    if data["VEHICLE TYPE CODE 1"][i] == "":
-        data['CANT_AUTOS'][i] = 0
-    elif data["VEHICLE TYPE CODE 2"][i] == "":
-        data['CANT_AUTOS'][i] = 1
-    elif data["VEHICLE TYPE CODE 3"][i] == "":
-        data['CANT_AUTOS'][i] = 2
-    elif data["VEHICLE TYPE CODE 4"][i] == "":
-        data['CANT_AUTOS'][i] = 3
-    elif data["VEHICLE TYPE CODE 5"][i] == "":
-        data['CANT_AUTOS'][i] = 4
-    
 # for use with dropdown
 original_data = data
 

8483e9604fff9b1a98a1ac5c6df5d3373ee5d68d
manuhanono <101122859+manuhanono@users.noreply.github.com>
2022-11-10 23:42:32 -0300

    Update ProyectoI.py

--- ProyectoI.py
+++ ProyectoI.py
@@ -55,7 +55,7 @@ st.header("Where are the most people injured in NYC?")
 st.map(data.query("lat <= 41 & lat > 39 & lon > -80 & lon < -70")[['lat', 'lon']].dropna(how="any"))
 
 st.header("Preg 1")
-fig = px.histogram(data, x="CANT", color = "WEEKDAY")
+fig = px.histogram(data, x="CANT", y="WEEKDAY", color = "PINJ")
 st.plotly_chart(fig, use_container_width=True)
 
 st.header("AA")
